7dc5f9e650351684b17a18fcec9b48412333c529
Matt Hanvelt <mhanvelt@mac.com>
2020-07-25 02:48:36 -0500

    Added a Size property and a means of initializing on creation of new buffer elements (on both constructor and resize)

--- RingBuffer/RingBuffer.cs
+++ RingBuffer/RingBuffer.cs
@@ -4,17 +4,24 @@ namespace RingBuffer
 {
 	public class RingBuffer<T>
 	{
+		public delegate T InitializeElementDelegate();
+
 		private static readonly int DefaultSize = 3;
 
 		private T[] buffer;
 		private int currentIndex = 0;
 		private object ringLock = new object();
 
-		public RingBuffer() : this(DefaultSize) { }
-		public RingBuffer(int size)
+		public RingBuffer() : this(DefaultSize, null) { }
+		public RingBuffer(int size) : this(size, null) { }
+		public RingBuffer(int size, InitializeElementDelegate initializer)
 		{
 			if(size < 1) { throw new ArgumentOutOfRangeException("size"); }
 			this.buffer = new T[size];
+			for(int x = 0; x < this.buffer.Length && initializer != null; x++)
+			{
+				this.buffer[x] = initializer.Invoke();
+			}
 		}
 
 		public T Current
@@ -47,18 +54,36 @@ namespace RingBuffer
 				}
 			}
 		}
+		public int Size
+		{
+			get
+			{
+				lock(this.ringLock)
+				{
+					return this.buffer.Length;
+				}
+			}
+		}
 
 		public RingBuffer<T> Resize(int newSize)
+		{
+			return this.Resize(newSize, null);
+		}
+		public RingBuffer<T> Resize(int newSize, InitializeElementDelegate initializer)
 		{
 			if(newSize < 1) { throw new ArgumentOutOfRangeException("newSize"); }
 			lock(this.ringLock)
 			{
 				Array.Resize<T>(ref this.buffer, newSize);
-				if(this.currentIndex > this.buffer.Length - 1) { this.currentIndex = this.buffer.Length -1; }
+				if(this.currentIndex > this.buffer.Length - 1) { this.currentIndex = this.buffer.Length - 1; }
+				for(int x = 0; x < this.buffer.Length && initializer != null; x++)
+				{
+					this.buffer[x] = initializer.Invoke();
+				}
+
 				return this;
 			}
 		}
-
 		public RingBuffer<T> MoveNext()
 		{
 			lock(this.ringLock)
